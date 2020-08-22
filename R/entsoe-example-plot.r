d652b9c5dca622ead5184a990d270c25ca22c912
7 <cloud@rejustify.com>
2020-08-22 20:17:03 +0200

    Corrected plotting specs.

--- R/entsoe-example-plot.r
+++ R/entsoe-example-plot.r
@@ -2,21 +2,22 @@
 require(ggplot2)
 
 # convert to numbers
-rdf$data[,2:3] <- as.numeric(rdf$data[,2:3])
+rdf$data[,2] <- as.numeric(rdf$data[,2])
+rdf$data[,3] <- as.numeric(rdf$data[,3])
 
 # standardize values to January = 1
 for(i in nrow(rdf$data):1) {
   rdf$data[i,-1] <- rdf$data[i,-1] / rdf$data[rdf$data$month == paste0("01-",substring(rdf$data$month[[i]],4,7)),-1]
 }
 
-# plot for Italy
+# group the months and years
 rdf$data$year  <- substring(rdf$data$month,4,7)
 rdf$data$month <- substring(rdf$data$month,1,2)
+
+# plot for Italy
 ggplot(data=rdf$data, aes(x=month, y = `entsoe,Italy`, group = year)) +
   geom_line(aes(color=year))
 
-  # plot for UK
-  rdf$data$year  <- substring(rdf$data$month,4,7)
-  rdf$data$month <- substring(rdf$data$month,1,2)
-  ggplot(data=rdf$data, aes(x=month, y = `entsoe,UK`, group = year)) +
-    geom_line(aes(color=year))
+# plot for UK
+ggplot(data=rdf$data, aes(x=month, y = `entsoe,United Kingdom`, group = year)) +
+  geom_line(aes(color=year))
--- R/entsoe-example-setup.r
+++ R/entsoe-example-setup.r
@@ -6,7 +6,7 @@ setCurl(learn=TRUE)
 register(token = "YOUR_TOKEN", email = "YOUR_EMAIL")
 
 # ENTSOE data template
-df <- data.frame(month = format(seq(as.Date("2016-1-1"), as.Date("2020-6-1"), "months"), "%m-%Y"),
+df <- data.frame(month = format(seq(as.Date("2018-1-1"), as.Date("2020-6-1"), "months"), "%m-%Y"),
                  `entsoe,Italy`  = NA,
                  `entsoe,United Kingdom`  = NA,
                  check.names = FALSE, stringsAsFactors = FALSE)
